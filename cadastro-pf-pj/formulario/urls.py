df04cdfafe9774c2f238a85269b931de059de806
Peterson Batista Gomes <petersonbatista8@gmail.com>
2022-02-09 22:25:11 -0300

    Templates, Urls, views

--- cadastro-pf-pj/formulario/urls.py
+++ cadastro-pf-pj/formulario/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path(),
+    path('', views.lista_cadastro, name='lista-cadastro'),
+    path('novocadastro/', views.novo_cadastro, name='novo-cadastro'),
+    path('cadastro/<int:id>/', views.cadastro, name='cadastro'),
+    path('editar/<int:id>/', views.editar_cadastro, name='editar-cadastro'),
 ]
--- cadastro-pf-pj/formulario/views.py
+++ cadastro-pf-pj/formulario/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import FormPessoaFisica, FormPessoaJuridica
 
-# Create your views here.
+
+def lista_cadastro(request):
+    pass
+
+
+def novo_cadastro(request):
+    pass
+
+
+def cadastro(request):
+    pass
+
+
+def editar_cadastro(request):
+    pass
+
+
