3dd11d9a0b57d7c9518009040209cd7e22ce7344
Peterson Batista Gomes <petersonbatista8@gmail.com>
2022-02-18 21:49:31 -0300

    Função vercadastro

--- cadastro-pf-pj/cadastro/settings.py
+++ cadastro-pf-pj/cadastro/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- cadastro-pf-pj/formulario/views.py
+++ cadastro-pf-pj/formulario/views.py
@@ -6,9 +6,9 @@ from django.views import generic
 
 
 def lista_cadastro(request):
-    cliente = DadosPessoa.objects.all().order_by('nome')
+    cliente = DadosPessoa.objects.all().order_by('id')
 
-    return render(request, 'cadastros/lista_cad.html', {'cliente': cliente})
+    return render(request, 'cadastro/lista_cad.html', {'cliente': cliente})
 
 
 def novo_cadastro_pf(request):
@@ -23,8 +23,7 @@ def novo_cadastro_pf(request):
 
     else:
         form_pf = FormPessoaFisica
-
-        return render(request, 'cadastros/cadastro_pf.html', {'form': form_pf})
+        return render(request, 'cadastro/cadastro_pf.html', {'form': form_pf})
 
 
 def novo_cadastro_pj(request):
@@ -39,13 +38,35 @@ def novo_cadastro_pj(request):
 
     else:
         form_pj = FormPessoaJuridica
-
-        return render(request, 'cadastros/cadastro_pf.html', {'form': form_pj})
-
-
-def cadastro(request):
-    pass
+        return render(request, 'cadastro/cadastro_pf.html', {'form': form_pj})
 
 
-def editar_cadastro(request):
-    pass
+def editar_cadastro_pf(request, id):
+    cad = get_object_or_404(DadosPessoa, pk=id)
+    form_pj = FormPessoaJuridica(instance=cad)
+
+    if request.method == 'POST':
+        form_pj = FormPessoaJuridica(request.POST, instance=cad)
+
+        if not form_pj.is_valid():
+            return render(request, 'cadastro/edit_pj.html', {'form': form_pj})
+
+        else:
+            cad.save()
+            return redirect('/')
+
+    else:
+        return render(request, 'cadastro/edit_pj.html', {'form': form_pj})
+
+
+def vercadastro(request, id):
+    cad = get_object_or_404(DadosPessoa, pk=id)
+
+    if DadosPessoa:
+        form_pf = FormPessoaFisica(instance=cad)
+        return render(request, 'cadastro/cadastro.html', {'form': form_pf})
+
+    else:
+        form_pj = FormPessoaJuridica(instance=cad)
+        return render(request, 'cadastro/cadastro.html', {'form': form_pj})
+
