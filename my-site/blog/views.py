1213e63c9d2ac82a3b2420a56ea3417e95ed80b1
Peterson Batista Gomes <petersonbatista8@gmail.com>
2022-03-29 22:53:24 -0300

    Add tags

--- my-site/blog/views.py
+++ my-site/blog/views.py
@@ -4,15 +4,40 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
+from taggit.models import Tag
+
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
-class PostListView(ListView):
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
+
+
+'''class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'post/list.html'
+'''
 
 
 def post_detail(request, year, month, day, post):
